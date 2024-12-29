93ea7cdf9c0c4660cbd18119b03571df7e586271
Nirusha Srinivas <nirushasrinivas23@gmail.com>
2024-12-29 20:09:43 +0530

    cloudinary js file chnages

--- server/helpers/cloudinary.js
+++ server/helpers/cloudinary.js
@@ -7,18 +7,24 @@ cloudinary.config({
   cloud_name: "devoc32yu",
   api_key: "127971356599795",
   api_secret: "hqn4-Gp_6vQ9hsc7Joay4ycA7IQ",
+  secure: true, 
 });
 
 const storage = new multer.memoryStorage();
 
 async function imageUploadUtil(file) {
-  const result = await cloudinary.uploader.upload(file, {
-    resource_type: "auto",
-  });
+  try {
+    const result = await cloudinary.uploader.upload(file, {
+      resource_type: "auto", 
+      secure: true, 
+    });
 
-  return result;
+    return result; 
+  } catch (error) {
+    console.error("Error uploading to Cloudinary:", error);
+    throw new Error("Failed to upload image to Cloudinary");
+  }
 }
-
 const upload = multer({ storage });
 
 module.exports = { upload, imageUploadUtil };
